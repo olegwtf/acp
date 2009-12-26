f5dc0fd29985fb21901f213c0a499139fbe7e180
omgwtf <omgwtf@e71d12fa-488e-4c5a-8862-ef258195d0c4>
2009-12-26 15:37:16 +0000

    git-svn-id: http://svn.xp-dev.com/svn/acp@10 e71d12fa-488e-4c5a-8862-ef258195d0c4

--- WWW/AOstat.pm
+++ WWW/AOstat.pm
@@ -53,6 +53,9 @@ sub stat
 	my ($self) = @_;
 	
 	my $page = $self->geturl(URL_STAT);
+	$page =~ /ERR=(\d+)/;
+	return () if $1 != 0;
+	
 	my ($traff) = $page =~ /REMAINS_MB=(\d+)/;
 	my ($money) = $page =~ /REMAINS_RUR=(\d+(?:.\d{1,2})?)/;
 	my $status  = index($page, ';OFF') == -1
@@ -122,9 +125,143 @@ sub credit
 
 sub geturl
 {
-	my ($self, $url) = @_;
+	my ($self, $url, $anonym) = @_;
 	
-	return $self->{ua}->get($url, Authorization=>"Basic ".encode_base64("$self->{login}:$self->{password}"))->content;
+	return $self->{ua}->get($url, $anonym ? undef : Authorization=>"Basic ".encode_base64("$self->{login}:$self->{password}"))->content;
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+WWW::AOstat - Implementation of the stat.academ.org API
+
+=head1 SYNOPSIS
+
+ use WWW::AOstat;
+ 
+ my $stat = WWW::AOstat->new(timeout=>10);
+ if($stat->login('login', 'password'))
+ {
+	$stat->turn(1);
+	my ($money, $traffic, $status) = $stat->stat();
+ }
+ else
+ {
+	die('Login failed');
+ }
+
+=head1 DESCRIPTION
+
+The C<WWW::AOstat> is a class implementing stat.academ.org API (only useful parts of the API).
+Some useful things made passing over the API, because of its incompleteness. With this module
+you can login to stat.academ.org, turn on/off internet, get actual balance and online status.
+Also you can get present traffic/money usage, amount of the credit (if you use it) and number
+of days remains.
+
+=head1 METHODS
+
+=over
+
+=item $stat->new(%lwp_opts)
+
+Constructs new C<WWW::AOstat> object. You can pass options pairs as argument. This options
+would pass to LWP::UserAgent constructor.
+
+Example:
+
+	$stat = WWW::AOstat->new(timeout=>10, agent=>'Mozilla 5.0');
+
+=item $stat->login($login, $password , $nocheck_for_success)
+
+Trying to login to stat.academ.org . If optional parameter $nocheck_for_success set to true
+login method will not try to real login and will return true even if login or password is 
+incorrect. Otherwise return true on success and false on failure.
+
+Example:
+
+	$stat->login('qwerty', '1234') or die('Login failed');
+
+=item $stat->stat()
+
+Trying to get user statistic. Return list of the traffic remained, money remained and online
+status on success. On failure return empty list, which means false in the list context.
+
+Example:
+
+	if(my ($traff, $money, $status) = $stat->stat)
+	{
+		print "Statistic: traffic - $traff, money - $money, status - "
+		      .($status == 1 ? "online" : $status == 0 ? "offline" : "inaccessible");
+	}
+	else
+	{
+		die("Failed to get stat");
+	}
+
+=item $stat->turn($act)
+
+Trying to turn on the internet if argument is true and turn off if argument is false.
+Return true on success and false on failure.
+
+=item $stat->usage($regexp)
+
+Trying to get present traffic and money usage. Return list of the traffic and money on
+success. On failure return empty list, which means false in the list context. You can
+pass optional parameter which should be regular expression if you want to override
+default regular expression (for example if it is not already work).
+
+Example:
+
+	if(my ($traff, $money) = $stat->usage)
+	{
+		print "Usage: traffic - $tarff, money - $money";
+	}
+	else
+	{
+		die("Failed to get usage");
+	}
+
+=item $stat->credit($regexp)
+
+Getting credit amount and days remains. If credit used it return list of the credit
+amount and days remains. You can pass optional parameter which should be regular
+expression if you want to override default regular expression
+(for example if it is not already work).
+
+Example:
+
+	if(my ($cred_sum, $cred_time) = $stat->credit)
+	{
+		print "You have credit $cred_sum ($cred_time)";
+	}
+	else
+	{
+		print "You have not credit";
+	}
+
+=item $stat->geturl($url , $anonym)
+
+For private usage. But you can use it to get some external page which is not
+implemented by this module. It use GET method with Basic authorization which
+use your current login and password. If optional second parameter set to true
+it will not use Basic authorization.
+
+Example:
+
+	my $page = $stat->geturl('http://google.ru', 1);
+
+=back
+
+=head1 SEE ALSO
+
+L<LWP::UserAgent>
+
+=head1 COPYRIGHT
+
+Copyright 2009 Oleg G <verdrehung@gmail.com>.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
